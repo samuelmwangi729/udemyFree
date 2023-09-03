3edeb2b6a1a5647e26e6fb7aa17076c7e2404314
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2023-09-03 20:31:05 +0300

    User authenication tried

--- controllers/Users/Users.js
+++ controllers/Users/Users.js
@@ -6,12 +6,21 @@
 //import the user model
 const User = require('../../models/Users/User')
 
-exports.Register = async(req,res)=>{
-    console.log(req.body)
-    const user = new User({
-        //then check for the details here
-    })
-    res.status(201).json({
-        message:'Register the user'
-    })
+module.exports.Register = async(req,res)=>{
+    try{
+        const user = User.create({
+            //then check for the details here
+            firstName:req.body.firstName,
+            lastName:req.body.lastName,
+            username:req.body.username,
+            email:req.body.email,
+        })
+        if(user){
+            //
+        }else{
+            console.log('error happened')
+        }
+    }catch(e){
+        console.log('could not create user')
+    }
 }
