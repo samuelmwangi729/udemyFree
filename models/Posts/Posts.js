cd2705201d2dfe80c2b64b4049c8aa073e33f9fb
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2023-09-03 10:50:16 +0300

    Posts Data Model Done

---
+++ models/Posts/Posts.js
@@ -0,0 +1,56 @@
+const mongoose  =  require('mongoose')
+const User = require('../Users/User')
+const postSchema = mongoose.Schema({
+    title:{
+        type:String,
+        required:[true,'Post title Is Required']
+    },
+    postImage:{
+        type:String,
+        default:''
+    },
+    postContent:{
+        type:String,
+        required:[true,'Content Is required']
+    },
+    Shares:{
+        type:Number,
+        default:0
+    },
+    Author:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
+    },
+    postViews:{
+        type:Number,
+        default:0
+    },
+    postLikes:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }
+    ],
+    postDislikes:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }
+    ],
+    postComments:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Comment'
+        }
+    ],
+    Category:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'Category'
+    },
+    schedulePost:{
+        type:Date,
+        default:null
+    },
+},{timestamps:true})
